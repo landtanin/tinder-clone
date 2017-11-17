2d7670fbb97c6ca7b6547a94bd5639a5f8b2e2c5
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-17 07:39:49 +0000

    create MatchesViewControl and segue back and forward to this view

---
+++ Tinder-clone/MatchesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MatchesViewController.swift
+//  Tinder-clone
+//
+//  Created by Tanin on 17/11/2017.
+//  Copyright © 2017 landtanin. All rights reserved.
+//
+
+import UIKit
+
+class MatchesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backTapped(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+  
+}
