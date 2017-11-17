8923cbf0dcc3f7bd7f87b621b46f2837c2391e46
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-17 07:57:56 +0000

    got specimen list of user

--- Tinder-clone/MatchesViewController.swift
+++ Tinder-clone/MatchesViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class MatchesViewController: UIViewController {
-
+class MatchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "matchCell", for: indexPath)
+        return cell
+        
+    }
+    
     @IBAction func backTapped(_ sender: UIBarButtonItem) {
         
         dismiss(animated: true, completion: nil)
         
     }
-  
+    
 }
